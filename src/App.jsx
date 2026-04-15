479753b6a3100ca716f36c46ee91cdcd71afd501
Julia Vacaru <julia.vacaru@student.unitbv.ro>
2026-04-15 14:59:37 +0300

    Ex 2 complet: Adaugat Navbar si configurat App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './pages/Home';
+import Navbar from './Navbar';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 function App() {
  return (
  <BrowserRouter>
+ <Navbar />
  <Routes>
  <Route path="/" element={<Home />} />
  <Route path="/projects" element={<Projects />} />
